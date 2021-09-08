196af2a22b66b08b23a3de7ef8a30ebccc0b5e47
fabricio <fabricio.jacob@outlook.com>
2021-09-07 22:06:37 -0300

    add

---
+++ provider-service/src/test/kotlin/com/github/fabriciolfj/providerservice/ProviderServiceApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.github.fabriciolfj.providerservice
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ProviderServiceApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
